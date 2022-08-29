05572556870ebf87f3353a6c118f53a8d0b5d539
JDPT93 <josedanielpereztorres@gmail.com>
2022-08-29 17:17:35 -0500

    modified:   src/main/java/com/jdpt93/cycle3/backend/services/MatchService.java

--- src/main/java/com/jdpt93/cycle3/backend/services/MatchService.java
+++ src/main/java/com/jdpt93/cycle3/backend/services/MatchService.java
@@ -3,7 +3,9 @@ package com.jdpt93.cycle3.backend.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.jdpt93.cycle3.backend.converters.MatchConverter;
 import com.jdpt93.cycle3.backend.data.MatchData;
@@ -22,6 +24,8 @@ public class MatchService {
     }
 
     public MatchData save(MatchData match) {
+        if (match.getLocalTeam().getId() == match.getGuestTeam().getId())
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Local Team and Guest Team must be different");
         return matchConverter.toData(matchRepository.save(matchConverter.toEntity(match)));
     }
 
